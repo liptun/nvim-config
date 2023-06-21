d37ada21c27e057f20cb9d9fba815114acffc6b5
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-06-21 08:42:57 +0200

    add new remaps

--- lua/liptun/remap.lua
+++ lua/liptun/remap.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "<leader>pp", function() vim.cmd("set paste!") end)
 vim.keymap.set("n", "<leader>r", function() vim.cmd("set relativenumber!") end)
 vim.keymap.set("n", "<leader>h", function() vim.cmd("noh") end)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
