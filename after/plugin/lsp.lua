f3624b2f40191f3591f30d14fac627f8cba384b2
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-06-29 09:48:48 +0200

    config lua ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,7 +28,24 @@ end)
 lspconfig.tsserver.setup({})
 lspconfig.eslint.setup({})
 
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 lspconfig.jsonls.setup {
   settings = {
