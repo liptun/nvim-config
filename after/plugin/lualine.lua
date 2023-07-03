8f3cf8a203ad1f6c289fb8844ffe9bcb6d523602
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-07-03 12:39:25 +0200

    lualine styles

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -20,7 +20,7 @@ require('lualine').setup {
     icons_enabled = false,
     theme = custom_gruvbox,
     component_separators = { left = '|', right = '|' },
-    section_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
