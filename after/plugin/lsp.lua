4944e605992ca49b6c74918c1436214089c0f9d9
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-07-03 10:17:44 +0200

    custom icons

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require('lsp-zero').preset({})
 
 local lspconfig = require("lspconfig")
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
   lsp.default_keymaps({ buffer = bufnr })
 
   local opts = { buffer = bufnr, remap = false }
@@ -69,14 +69,30 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ["<C-Space>"] = cmp.mapping.complete()
 })
 
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      maxwidth = 50,
+      ellipsis_char = '...',
+    })
+  }
+}
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
+})
+
+lsp.set_sign_icons({
+  error = '',
+  warn = '',
+  hint = '󰉀',
+  info = '󰙎'
 })
 
 lsp.setup()
