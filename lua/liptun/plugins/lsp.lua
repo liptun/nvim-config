ee7c493f9077cdbac3599daad3011e86196c2335
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2025-09-26 23:00:52 +0200

    simple lsp

--- lua/liptun/plugins/lsp.lua
+++ lua/liptun/plugins/lsp.lua
@@ -1,20 +1,34 @@
-require('mason').setup({})
-require('mason-lspconfig').setup({
+require("mason").setup({})
+
+require("mason-lspconfig").setup({
+    ensure_installed = { "ts_ls", "eslint", "jsonls", "html", "cssls", "lua_ls", "tailwindcss" },
+    automatic_installation = true,
     handlers = {
         function(server_name)
-            -- default servers
             vim.lsp.start(vim.lsp.config[server_name])
         end,
         jsonls = function()
             vim.lsp.config.jsonls = {
                 settings = {
                     json = {
-                        schemas = require('schemastore').json.schemas(),
+                        schemas = require("schemastore").json.schemas(),
                         validate = { enable = true },
                     },
-                }
+                },
             }
             vim.lsp.start(vim.lsp.config.jsonls)
+        end,
+        lua_ls = function()
+            vim.lsp.config.lua_ls = {
+                settings = {
+                    Lua = {
+                        runtime = { version = "LuaJIT" },
+                        diagnostics = { globals = { "vim" } },
+                        telemetry = { enable = false },
+                    },
+                },
+            }
+            vim.lsp.start(vim.lsp.config.lua_ls)
         end,
     },
 })
--- lua/liptun/plugins/telescope.lua
+++ lua/liptun/plugins/telescope.lua
@@ -1,7 +1,7 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
-telescope.setup{}
+telescope.setup {}
 
 -- vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
@@ -9,3 +9,6 @@ vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "Live grep" })
 -- vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 -- vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+vim.keymap.set("n", "gr", function()
+    require("telescope.builtin").lsp_references({ prompt_title = "LSP References" })
+end, opts)
