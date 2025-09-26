b1cf4ff57adeab2898bbf9bee9d5afb708ade0e7
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2025-09-26 21:32:41 +0200

    bootstraping new config for 2025

--- core/keymaps.lua
+++ core/keymaps.lua
@@ -1,19 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>px", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>pp", function() vim.cmd("set paste!") end)
 vim.keymap.set("n", "<leader>r", function() vim.cmd("set relativenumber!") end)
 vim.keymap.set("n", "<leader>h", function() vim.cmd("noh") end)
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
-vim.keymap.set("n", "<leader>t", function() vim.cmd("terminal") end)
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end)
 vim.keymap.set("n", "<leader>K", function() vim.diagnostic.open_float() end)
--- init.lua
+++ init.lua
@@ -1,2 +1,37 @@
-require("liptun")
-require("plugins")
+require("core.options")
+require("core.keymaps")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "folke/lazy.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("plugins.gruvbox")
+    end
+  },
+  {
+        'ThePrimeagen/harpoon',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require("plugins.harpoon")
+        end
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("plugins.telescope")
+    end
+  }
+})
